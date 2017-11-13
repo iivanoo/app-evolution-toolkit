617afddfe65d7f8ae6e18fa783af594852b9c317
Christopher Ras <chris@MacBook-Pro-2.local>
2017-11-13 11:43:27 +0100

    Added iOS app

---
+++ SP2/IOS/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Christopher Ras on 13/11/2017.
+//  Copyright © 2017 Chris Ras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SP2/IOS/HelloWorld/build/HelloWorld.build/Debug-iphonesimulator/HelloWorld.build/Objects-normal/i386/AppDelegate.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/chris/Downloads/app-evolution-toolkit/SP2/IOS/HelloWorld/HelloWorld/AppDelegate.m \
+  /Users/chris/Downloads/app-evolution-toolkit/SP2/IOS/HelloWorld/HelloWorld/AppDelegate.h
