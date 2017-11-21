c6b6dd564e401d9d4ea1ef11c229eed1f37a1c9a
Christopher Ras <chris@MacBook-Pro-2.local>
2017-11-21 15:16:23 +0100

    Bugs are now written to csv file

--- SP2/InferTool.py
+++ SP2/InferTool.py
@@ -5,7 +5,7 @@ Assumes Gradle build. SDK folder must be defined in local.properties in
 the app folder. SDK and Build tools versions must match the app.
 """
 
-import os, sys, subprocess, shutil
+import os, sys, subprocess, shutil, csv
 
 # Constants
 LOCALPROPERTIES = "local.properties"
@@ -60,13 +60,52 @@ def removePreviousBuild() :
 	if os.path.isdir(BUILDDIRECTORY):
 		shutil.rmtree(BUILDDIRECTORY, ignore_errors=False, onerror=None)	
 
+# reads the bug report, prepares it so it can be exported to csv file
 def readBugReport():
 	os.chdir(BUGDIRECTORY)
 	if os.path.isfile(BUGFILE):
 		bugreport = open(BUGFILE, "r")
+		bugsToCSVArray = []
 		bugreportText = bugreport.read()
+
+		# splits bugs.txt so that only the bugs are in the array
 		splitReport = bugreportText.split('\n\n')
-		print(splitReport[1])
+		splitReport = splitReport[1:-2]
+
+		# index for unique id
+		bugIndex = 1
+
+		# separates the bug into different parts
+		for bug in splitReport:
+			bugsArray = bug.split('\n')
+			bugBundle = bugsArray[0].split(" error: ")
+			bugPath = bugBundle[0].split(":")
+
+			# Bug File path
+			filePath = bugPath[0]
+			
+			# Bug line number
+			lineNumber = bugPath[1]
+			
+			# Bug type
+			bugType = bugBundle[1]
+			
+			# Bug description
+			bugDescription = bugsArray[1]
+
+			bugsToCSVArray.append([str(bugIndex), bugType, filePath, lineNumber, bugDescription])
+
+
+			bugIndex+=1
+		writeBugsToCSV(bugsToCSVArray)
+
+
+# writes the bugs to the csv file
+def writeBugsToCSV(bugs_array):
+	with open('app_bugs.csv', 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+		for bug in bugs_array:
+			writer.writerow(["ID: " + bug[0]] + ["BUG_TYPE: " + bug[1]] + ["FILE_PATH: " + bug[2]] + ["LINE_NUMBER: " + bug[3]] + ["BUG_DESCRIPTION: " + bug[4]])
 
 if __name__ == '__main__':
     main()
