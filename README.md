b6139ce2548b3c7b7a3ba71f4bce4e554362e674
Bob van den Berg <bobberg90@hotmail.com>
2018-07-07 08:42:47 +0200

    Added requirements.txt, made README.md and made a separate function for cloning repositories from a csv.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # app-evolution-toolkit
+
+For installing the Python dependencies a requirements.txt is included in the repository. The dependencies can be installed using the package manager pip:
+
+pip3 install -r requirements.txt
+
+For installing the Android SDK’s, Build-tools and platform tools (normally included in the Android Studio installation), simply run the sdk_installer.py script included in the repository.
+
+The repositories to be analysed can be manually cloned or moved to the repo_subfolder.
+
+The static analysis tool can be changed by changing out the static_analyzer_success variable. The output will need to be the same as the format used with Infer.
+- The output should be a .csv outputted in the folder repo_subfolder/<author> in the following format: <repository_name>_<commit_index>.csv.
+- The data of the csv itself that is outputted should be the following strings per line:  ID, BUG_TYPE, FILE_PATH, LINE_NUMBER, BUG_DESCRIPTION
+
+Then main.py can be run
--- SP1/main.py
+++ SP1/main.py
@@ -55,28 +55,8 @@ extensions = [
             ]
 
 
-# Download GitHub repositories from a csv row:
-def read_csv_and_clone_github_repositories():
-    with open('csv/example_github_repos.csv', "rt", encoding="utf8") as csv_file:
-        # Csv file rows are cut into pieces using ';'. Change if needed.
-        csv_file_data = csv.reader(csv_file, delimiter=";")
-        url = []
-        path = []
-        for rows in csv_file_data:
-            # In example_github_repos.csv file the repositories are stored in the 0 position in a row. Change if needed.
-            repository = rows[0]
-            # Put urls and paths in lists.
-            # Append with the repository extension and set the repository path in the repo_subfolder.
-            url.append('git://github.com/' + repository + '.git')
-            repository_path = 'repo_subfolder/' + repository
-            path.append(repository_path)
-    clone_repositories(url, path)
 
 
-def clone_repositories(url, path):
-    # Skip first row (header) and clone the repositories. (Max 100 per run allowed by GitHub)
-    for i in range(2, 3):
-        Repo.clone_from(url[i], path[i])
 
 
 fieldnames = [
